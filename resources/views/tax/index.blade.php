41239f859d81da082962251ea6b623b505c420ef
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2024-12-11 16:53:06 -0600

    VIsta Tax Agregada

---
+++ resources/views/tax/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+@section('indexcondition')
+
+<div class="container">
+ @if(Session::has('message'))
+ {{Session::get('message')}}
+ @endif   
+
+
+ <button type="button" class="btn btn-outline-success mb-3 mt-3 m-2"> <a class="text-dark" href="{{ url('impuestos/') }}">
+    Regresar
+</a> </button> 
+
+<button type="button" class="btn btn-outline-success mb-3 mt-3"> <a class="text-dark" href="{{ url('impuestos/create') }}">
+    Agregar Impuesto
+</a> </button> 
+
+
+<div class="container">
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>ID</th>
+            <th>IMPUESTO</th>
+            <th>VALOR</th>
+            <th>Opciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($taxes as $tax)
+        <tr>
+            <td>{{ $tax->id }}</td>
+            <td>{{ $tax->concept }}</td>
+            <td>{{ $tax->percentage }}</td>
+            <td>
+               <button class="btn btn-warning mb-2"> <a class="text-white" href="{{ url('impuestos/'.$tax->id.'/edit') }}">
+                    Editar
+                </a> </button> 
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{!! $taxes->links() !!}
+</div>
+</div>
+
+@endsection
