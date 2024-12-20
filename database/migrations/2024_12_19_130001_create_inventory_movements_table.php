0335272f4a594715511905040f9fcdfa8638e2b7
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2024-12-20 12:55:32 -0600

    Perfiles de Bianca y Perla

---
+++ database/migrations/2024_12_19_130001_create_inventory_movements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_movements', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->enum('type', ['Entrada', 'Salida', 'Ajuste'])->default('Entrada');
+            $table->integer('quantity');
+            $table->text('reason')->nullable(); // Motivo de ajuste o salida.
+            $table->unsignedBigInteger('reference_id')->nullable(); // Relación con la OC o Requisición.
+            $table->enum('source', ['Orden Compra', 'Requisición', 'Ajuste Manual'])->default('Ajuste Manual');
+            $table->timestamps();
+        
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_movements');
+    }
+};
