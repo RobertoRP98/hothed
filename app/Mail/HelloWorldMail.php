5b148a575158d1e50fb005c8aef4179534c90d26
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2024-12-23 14:42:23 -0600

    Test Email v.1.2

--- app/Mail/HelloWorldMail.php
+++ app/Mail/HelloWorldMail.php
@@ -37,7 +37,7 @@ class HelloWorldMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.hello',
         );
     }
 
