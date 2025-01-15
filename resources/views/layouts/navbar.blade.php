e9649c976347e7ef550c67f5b0b3e448d1ec529e
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2025-01-15 03:22:00 -0600

    Se agrega navbar a todos los usuarios basado en roles 1.3

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,34 +24,32 @@
             @endauth
 
             @auth
+            @php
+                // Determinar la ruta según el rol del usuario
+                $comprasUrl = '#'; // Enlace por defecto
+                if (Auth::user()->hasRole(['Developer', 'RespCompras'])) {
+                    $comprasUrl = '/requisiciones';
+                } elseif (Auth::user()->hasRole('AdmCompras')) {
+                    $comprasUrl = '/requisiciones-adm';
+                } elseif (Auth::user()->hasRole('OpeCompras')) {
+                    $comprasUrl = '/requisiciones-ope';
+                } elseif (Auth::user()->hasRole('ClientCompras')) {
+                    $comprasUrl = '/mis-requisiciones';
+                }
+            @endphp
+        
+            <li class="nav-item">
+                <a class="nav-link text-white" href="{{ url($comprasUrl) }}">Compras</a>
+            </li>
+        @endauth
+
+            @auth
             @if(Auth::user()->hasRole(['Developer', 'AdministracionKarla']))
                 <li class="nav-item">
                     <a class="nav-link text-white" href="{{ url('/register') }}">Registrar personal</a>
                 </li>
             @endif
         @endauth
-
-        @auth
-    @php
-        // Determinar la ruta según el rol del usuario
-        $comprasUrl = '#'; // Enlace por defecto
-        if (Auth::user()->hasRole(['Developer', 'RespCompras'])) {
-            $comprasUrl = '/requisiciones';
-        } elseif (Auth::user()->hasRole('AdmCompras')) {
-            $comprasUrl = '/requisiciones-adm';
-        } elseif (Auth::user()->hasRole('OpeCompras')) {
-            $comprasUrl = '/requisiciones-ope';
-        } elseif (Auth::user()->hasRole('ClientCompras')) {
-            $comprasUrl = '/mis-requisiciones';
-        }
-    @endphp
-
-    <li class="nav-item">
-        <a class="nav-link text-white" href="{{ url($comprasUrl) }}">Compras</a>
-    </li>
-@endauth
-
-
 
     @auth
     @if(Auth::user()->hasRole(['Almacen','Developer']))
