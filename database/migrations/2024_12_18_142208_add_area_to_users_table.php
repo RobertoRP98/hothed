deece2a912abe6ced7cb22a92e8afe1b3723660c
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2025-02-09 21:31:18 -0600

    Se duerme a area para que corran las migraciones

--- database/migrations/2024_12_18_142208_add_area_to_users_table.php
+++ database/migrations/2024_12_18_142208_add_area_to_users_table.php
@@ -4,29 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('area',
-            ['SGI', 'HSE', 'VENTAS', 'LOGISTICA', 'ALMACEN', 'MANTENIMIENTO HTTAS','OPERACIONES', 
-            'RECURSOS HUMANOS','TI','CONTRATOS Y COBRANZA','CONTABILIDAD','COMPRAS E INDIRECTOS',
-            'ADMINISTRACION','MANTENIMIENTO INFRAESTRUCTURA','SERVICIOS','FONDO FIJO']
-            )->after('departament');
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//     //  */
+//     // public function up(): void
+//     // {
+//     //     Schema::table('users', function (Blueprint $table) {
+//     //         $table->enum('area',
+//     //         ['SGI', 'HSE', 'VENTAS', 'LOGISTICA', 'ALMACEN', 'MANTENIMIENTO HTTAS','OPERACIONES', 
+//     //         'RECURSOS HUMANOS','TI','CONTRATOS Y COBRANZA','CONTABILIDAD','COMPRAS E INDIRECTOS',
+//     //         'ADMINISTRACION','MANTENIMIENTO INFRAESTRUCTURA','SERVICIOS','FONDO FIJO']
+//     //         )->after('departament');
+//     //     });
+//     // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
-};
+//     // /**
+//     //  * Reverse the migrations.
+//     //  */
+//     // public function down(): void
+//     // {
+//     //     Schema::table('users', function (Blueprint $table) {
+//     //         //
+//     //     });
+//     // }
+//};
