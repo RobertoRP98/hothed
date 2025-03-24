791897114ce14506d3c13b9777565eef801a38ae
RobertoRP98 <100012114+RobertoRP98@users.noreply.github.com>
2025-03-24 12:30:47 -0600

    SE COMENTA ESTA MIGRACION PARA PODER AVANZAR EN EL DESPLIEGUE

--- database/migrations/2024_12_10_123032_add_area_to_users_table.php
+++ database/migrations/2024_12_10_123032_add_area_to_users_table.php
@@ -9,40 +9,40 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum(
-                'area',
-                [
-                    'SGI', // auxiliar de SGI - Resp de SGI
-                    'HSE', // auxiliar de HSE - Coord HSE 
-                    'VENTAS', //Auxiliar de Ventas - Coord de ventas
-                    'LOGISTICA', //Auxiliar de Logistica - Coord de Logistica
-                    'ALMACEN', //Auxiliar de Almacen  - Coord de Almacen
-                    'MANTENIMIENTO HTTAS', //Auxiliar de Mantenimiento - Coord de Manto
-                    'OPERACIONES', //Auxiliar de Operaciones - Coord de Operaciones 
-                    'RECURSOS HUMANOS', //Auxiliar de RH - Coord de RH
-                    'TI', //AUXILIAR DE TI - Responsable de TI
-                    'CONTRATOS Y COBRANZA', //AUX DE CON Y COB- COORD CONTRATOS
-                    'CONTABILIDAD', //AUX DE CONTABILIDAD -  coor contabilidad
-                    'COMPRAS E INDIRECTOS', //AUX DE COMPRAS - Resp de compras
-                    'ADMINISTRACION', //AUX DE ADM - Coord adm
-                    'MANTENIMIENTO INFRAESTRUCTURA', //RESP DE MANTO INFRA
-                    'SERVICIOS', //AUX DE SERVICIOS  - Resp de servicios
-                    'FONDO FIJO' // RESP FONDO JIJO
-                ]
-            )->after('departament')->default('TI');;
-        });
-    }
+    // public function up(): void
+    // {
+    //     Schema::table('users', function (Blueprint $table) {
+    //         $table->enum(
+    //             'area',
+    //             [
+    //                 'SGI', // auxiliar de SGI - Resp de SGI
+    //                 'HSE', // auxiliar de HSE - Coord HSE 
+    //                 'VENTAS', //Auxiliar de Ventas - Coord de ventas
+    //                 'LOGISTICA', //Auxiliar de Logistica - Coord de Logistica
+    //                 'ALMACEN', //Auxiliar de Almacen  - Coord de Almacen
+    //                 'MANTENIMIENTO HTTAS', //Auxiliar de Mantenimiento - Coord de Manto
+    //                 'OPERACIONES', //Auxiliar de Operaciones - Coord de Operaciones 
+    //                 'RECURSOS HUMANOS', //Auxiliar de RH - Coord de RH
+    //                 'TI', //AUXILIAR DE TI - Responsable de TI
+    //                 'CONTRATOS Y COBRANZA', //AUX DE CON Y COB- COORD CONTRATOS
+    //                 'CONTABILIDAD', //AUX DE CONTABILIDAD -  coor contabilidad
+    //                 'COMPRAS E INDIRECTOS', //AUX DE COMPRAS - Resp de compras
+    //                 'ADMINISTRACION', //AUX DE ADM - Coord adm
+    //                 'MANTENIMIENTO INFRAESTRUCTURA', //RESP DE MANTO INFRA
+    //                 'SERVICIOS', //AUX DE SERVICIOS  - Resp de servicios
+    //                 'FONDO FIJO' // RESP FONDO JIJO
+    //             ]
+    //         )->after('departament')->default('TI');;
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::table('users', function (Blueprint $table) {
+    //         //
+    //     });
+    // }
 };
