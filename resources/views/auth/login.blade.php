55dda5720402ded771704bcbed64bfb6f7166f93
RobertoRP98 <robertorpsistemas@gmail.com>
2025-05-01 10:29:19 -0600

    Fixeo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
 
                     <div class="card-body">
                         <div class="col-md-12 d-flex justify-content-center">
-                                <img src="{{ asset('images/Logo-Azul-Gris.png') }}" alt="" width="400" height="100" class="col-md-4 col-form-label text-md-end">
+                                <img src="{{ asset('images/Logo-Azul-Gris.png') }}" alt=""  class="col-md-4 col-form-label text-md-end">
                         </div>
                         
                         <form method="POST" action="{{ route('login') }}">
