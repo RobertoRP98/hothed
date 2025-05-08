7d70395564fd1f27315ec4e85297315d7e23b72d
RobertoRP98 <robertorpsistemas@gmail.com>
2025-05-08 17:04:37 -0600

    Se crean migraciones y modelos

---
+++ database/migrations/2025_05_08_153340_create_documents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('version');
+            $table->foreignId('category_id')->nullable()->constrained('documents_categories')->nullOnDelete();
+            $table->boolean('download')->default(false);
+            $table->boolean('general')->default(false);
+            $table->string('file_path'); // antes "root"
+            $table->foreignId('revisor_id')->nullable()->constrained('users_sgi')->nullOnDelete();
+            $table->foreignId('aprobador_id')->nullable()->constrained('users_sgi')->nullOnDelete();
+            $table->foreignId('area_resp_id')->nullable()->constrained('areas_sgi')->nullOnDelete();
+            $table->enum('auth_1', ['PENDIENTE', 'AUTORIZADO', 'RECHAZADO'])->default('PENDIENTE'); // REVISOR
+            $table->enum('auth_2', ['PENDIENTE', 'AUTORIZADO', 'RECHAZADO'])->default('PENDIENTE'); // AUTORIZADOR
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
